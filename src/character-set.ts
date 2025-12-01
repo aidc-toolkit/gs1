83edd5a7aa429aa5818ce0e2e6ca9a2ff4f98aea
Kevin Dean <Kevin.Dean@datadevelopment.com>
2025-11-30 21:32:46 -0500

    Added GS1 AI encodable character set 64.

--- src/character-set.ts
+++ src/character-set.ts
@@ -1,4 +1,4 @@
-import { CharacterSetCreator, type CharacterSetValidator, Exclusions } from "@aidc-toolkit/utility";
+import { CharacterSetCreator, CharacterSetValidator, Exclusions } from "@aidc-toolkit/utility";
 
 /**
  * GS1 AI encodable character set 82 creator as defined in section 7.11 of the {@link https://www.gs1.org/genspecs | GS1
@@ -37,3 +37,18 @@ export const AI39_CREATOR = new CharacterSetCreator([
  * GS1 General Specifications}. Supports {@linkcode Exclusions.AllNumeric}.
  */
 export const AI39_VALIDATOR = AI39_CREATOR as CharacterSetValidator;
+
+/**
+ * GS1 AI encodable character set 64 validator as defined in section 7.11 of the {@link https://www.gs1.org/genspecs |
+ * GS1 General Specifications}. Doesn't support any exclusions.
+ */
+export const AI64_VALIDATOR = new CharacterSetValidator([
+    "-",
+    "0", "1", "2", "3", "4", "5", "6", "7", "8", "9",
+    "=",
+    "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M",
+    "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",
+    "_",
+    "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m",
+    "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"
+]);
